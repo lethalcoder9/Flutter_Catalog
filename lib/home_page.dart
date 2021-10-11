aace8af226ae7de0ae430763fcf0c9b59b085dc3
Utkarsh <utkarshpatil2001@gmail,com>
2021-10-11 09:53:55 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Utkarsh";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Flutter Catalog"),),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name!"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  int days = 30;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to flutter catalog!"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
