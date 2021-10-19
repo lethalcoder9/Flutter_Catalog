4a2151706e5b5e282ab45799fc00af46d35980f2
Utkarsh <utkarshpatil2001@gmail,com>
2021-10-19 18:08:18 +0530

    Day  10 completed

--- lib/utils/models/catalog.dart
+++ lib/utils/models/catalog.dart
@@ -1,17 +1,23 @@
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
   final String color;
   final String image;
 
-  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
 }
 
 final products = [
   Item(
-      id: "Codepur001",
+      id: 1,
       name: "iPhone 12 Pro",
       desc: "Apple iPhone 12th generation",
       price: 999,
